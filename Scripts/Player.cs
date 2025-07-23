f2d5a0525dad219101414995df3642121a37480d
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-07-24 00:31:10 +0545

    Added
    No Changed

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,4 +1,35 @@
 using System;
 using Godot;
 
-public partial class Player : CharacterBody2D { }
+public partial class Player : CharacterBody2D
+{
+    public enum State
+    {
+        Idle,
+        Running,
+        Attacking,
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+}
